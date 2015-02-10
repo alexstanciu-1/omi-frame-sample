9b33f5fa8ce8e3606fd136aee6631b94c20f83d9
Alex Stanciu <alex@omibit.com>
2015-02-10 12:04:29 +0200

    Initial

--- index.php
+++ index.php
@@ -4,18 +4,11 @@
 	
 	require("config.php");
 	
-	// this should be the default for production
-	error_reporting(E_ALL  & ~(E_STRICT | E_DEPRECATED | E_NOTICE | E_WARNING));
-	// this should be the default for development
-	// error_reporting(E_ALL  & ~(E_STRICT));
-	
 	// comment on this one a bit
-	// define('Q_FRAME_GET_ID_TYPE', "http://www.softdev.ro/~omibit/get_id_type.php");
+	// define('Q_FRAME_GET_ID_TYPE', "http://www.omiframe.com/API/types/");
 	
 	// include the frame
 	require_once("../omi-frame/src/init.php");
-	
-	// touch("code/view/order/OrderCtrl.edit.tpl");
 	
 	// include a module if needed, modules may also have a "include.php" script that will be included now
 	// \QAutoload::LoadModule("../../../../frame/modules/common/");
@@ -23,9 +16,11 @@
 	// set your code folder as the `running folder`
 	\QAutoload::AddMainFolder(Q_RUNNING_PATH."code/");
 
-	// only in development mode, remove or comment this line in production
+	// only if you need to run development mode, remove or comment this line in production
 	ini_set('memory_limit', '128M');
 	// Put your IP instead of "default" if you are not on the same network with the web server
+	// EnableDevelopmentMode() will only execute if the conditions are satisfied
+	// You may comment out this line in production
 	\QAutoload::EnableDevelopmentMode("default", false);
 		
 	// connect to the SQL server
